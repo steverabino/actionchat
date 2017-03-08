13d07e5c7d98c9d1d8db405c3b4db25be50b398f
Steve Smith <stevesmith2609@gmail.com>
2017-03-08 15:00:42 +0000

    Deletes all things “User” to be replaced by Devise

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  has_many :messages
-  has_many :chatrooms, through: :messages
-  validates :username, presence: true, uniqueness: true
-end
--- db/schema.rb
+++ db/schema.rb
@@ -31,10 +31,4 @@ ActiveRecord::Schema.define(version: 20170307164428) do
     t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
